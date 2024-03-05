e49b90455a3162321cbe7bf376daa6dde00f3cd2
Pierre SARINENA <pierre.sarinena@epfedu.fr>
2024-03-05 15:57:11 +0100

    fin carte eau

--- src/js/niveau2.js
+++ src/js/niveau2.js
@@ -1,3 +1,25 @@
+import * as fct from "/src/js/fonctions.js";
+
+
+
+// Variable globale
+var player; // désigne le sprite du joueur
+var clavier; // pour la gestion du clavier
+var droite;
+var gauche;
+var bas;
+var haut;
+var dash;
+var lent;
+var sprint;
+var vitesse_lent=0;
+var vitesse_dash=0;
+let image_sprint;
+var decor;
+var CalquedeTuiles;
+var rien;
+
+
 export default class niveau2 extends Phaser.Scene {
   // constructeur de la classe
   constructor() {
@@ -5,49 +27,239 @@ export default class niveau2 extends Phaser.Scene {
       key: "niveau2" //  ici on précise le nom de la classe en tant qu'identifiant
     });
   }
-  preload() {}
+
+  
+  preload() {
+    this.load.image("Phaser_JeuDeTuiles6", "src/assets/map_donjon_eau/arriere_plan.png");
+    this.load.tilemapTiledJSON("carte1", "src/assets/map_donjon_eau/map_fin_eau.json");
+  }
 
   create() {
-    this.add.image(400, 300, "img_ciel");
-    this.groupe_plateformes = this.physics.add.staticGroup();
-    this.groupe_plateformes.create(200, 584, "img_plateforme");
-    this.groupe_plateformes.create(600, 584, "img_plateforme");
+    fct.doNothing();
+    fct.doAlsoNothing();
+    const carteDuNiveau = this.add.tilemap("carte1");
+    const tileset = carteDuNiveau.addTilesetImage(  
+      "arriere_plan",
+      "Phaser_JeuDeTuiles6"
+    ); 
+    decor = carteDuNiveau.createLayer(
+      "decor",
+      tileset
+    );
+    CalquedeTuiles = carteDuNiveau.createLayer(
+      "CalquedeTuiles",
+      tileset
+    );
+    rien = carteDuNiveau.createLayer(
+      "rien",
+      tileset
+    );
+
+
+    CalquedeTuiles.setCollisionByProperty({ estSolide: true }); 
+
+
+
+
     // ajout d'un texte distintcif  du niveau
     this.add.text(400, 100, "Vous êtes dans le niveau 2", {
       fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif',
       fontSize: "22pt"
     });
 
-    this.porte_retour = this.physics.add.staticSprite(100, 550, "img_porte2");
 
-    this.player = this.physics.add.sprite(100, 450, "img_perso");
-    this.player.refreshBody();
-    this.player.setBounce(0.2);
-    this.player.setCollideWorldBounds(true);
-    this.clavier = this.input.keyboard.createCursorKeys();
-    this.physics.add.collider(this.player, this.groupe_plateformes);
+    // Création icone dash
+    image_sprint = this.add.image(16, 16, "Sprinter_bleu");
+
+    // Création du joueur
+    player = this.physics.add.sprite(200, 300, "Personnage");
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+    player.peutDash = true;
+
+
+
+    this.physics.world.setBounds(0, 0, 1600, 1280);
+    //  ajout du champs de la caméra de taille identique à celle du monde
+    this.cameras.main.setBounds(0, 0, 1600, 1280);
+    // ancrage de la caméra sur le joueur
+    this.cameras.main.startFollow(player);
+
+
+   // Création du clavier
+   clavier = this.input.keyboard.createCursorKeys();
+   haut = this.input.keyboard.addKey("Z");
+   bas = this.input.keyboard.addKey("S");
+   gauche = this.input.keyboard.addKey("Q");
+   droite = this.input.keyboard.addKey("D");
+   dash = this.input.keyboard.addKey("shift");
+   lent= this.input.keyboard.addKey("space");
+   sprint = this.input.keyboard.addKey("shift");
+
+    this.physics.add.collider(player, decor); 
+    this.physics.add.collider(player, CalquedeTuiles); 
+    this.physics.add.collider(player, rien); 
+
   }
 
   update() {
-    if (this.clavier.left.isDown) {
-      this.player.setVelocityX(-160);
-      this.player.anims.play("anim_tourne_gauche", true);
-    } else if (this.clavier.right.isDown) {
-      this.player.setVelocityX(160);
-      this.player.anims.play("anim_tourne_droite", true);
-    } else {
-      this.player.setVelocityX(0);
-      this.player.anims.play("anim_face");
-    }
-    if (this.clavier.up.isDown && this.player.body.touching.down) {
-      this.player.setVelocityY(-330);
-    }
+    // Déplacement du joueur
 
-    if (Phaser.Input.Keyboard.JustDown(this.clavier.space) == true) {
-      if (this.physics.overlap(this.player, this.porte_retour)) {
-        console.log("niveau 3 : retour vers selection");
-        this.scene.switch("selection");
-      }
-    }
-  }
+    if(lent.isDown){
+     vitesse_lent=70;
+     if (dash.isDown && player.peutDash==true){
+       vitesse_dash=8000;
+       this.dash(player,image_sprint);}
+   }else if (sprint.isDown){
+     vitesse_dash=100;
+     if (dash.isDown && player.peutDash==true){
+       vitesse_dash=8000;
+       this.dash(player,image_sprint);}
+   }else if (dash.isDown && player.peutDash==true){
+     vitesse_dash=8000;
+     this.dash(player,image_sprint);
+   }else{
+     vitesse_dash=0;
+     vitesse_lent=0;
+   }
+
+   if (gauche.isDown) {
+     if (haut.isDown){
+       player.setVelocityX(-160+vitesse_lent-vitesse_dash);
+       player.setVelocityY(-160+vitesse_lent-vitesse_dash);
+     }else if(bas.isDown) {
+       player.setVelocityX(-160+vitesse_lent-vitesse_dash);
+       player.setVelocityY(160-vitesse_lent+vitesse_dash);
+     }else{
+       player.setVelocityX(-160+vitesse_lent-vitesse_dash);
+     }
+   }  else if (droite.isDown) {
+     if (haut.isDown){
+       player.setVelocityX(160-vitesse_lent+vitesse_dash);
+       player.setVelocityY(-160+vitesse_lent-vitesse_dash);
+     }else if(bas.isDown) {
+       player.setVelocityX(160-vitesse_lent+vitesse_dash);
+       player.setVelocityY(160-vitesse_lent+vitesse_dash);
+     }else{
+       player.setVelocityX(160-vitesse_lent+vitesse_dash);
+     }
+   } else if (bas.isDown) {
+     if (gauche.isDown){
+       player.setVelocityX(-160+vitesse_lent-vitesse_dash);
+       player.setVelocityY(160-vitesse_lent+vitesse_dash);
+     }else if(droite.isDown) {
+       player.setVelocityX(160-vitesse_lent+vitesse_dash);
+       player.setVelocityY(160-vitesse_lent+vitesse_dash);
+     }else{
+       player.setVelocityY(160-vitesse_lent+vitesse_dash);
+     }
+   } else if (haut.isDown) {
+     if (gauche.isDown){
+       player.setVelocityX(-160+vitesse_lent-vitesse_dash);
+       player.setVelocityY(-160+vitesse_lent-vitesse_dash);
+     }else if(droite.isDown) {
+       player.setVelocityX(160-vitesse_lent+vitesse_dash);
+       player.setVelocityY(-160+vitesse_lent-vitesse_dash);
+     }else{
+       player.setVelocityY(-160+vitesse_lent-vitesse_dash);
+     }
+   } else {
+     player.setVelocityX(0);
+     player.setVelocityY(0);
+     player.anims.play("Personnage");
+   }
+
+   // Tir de la balle suivant la position de la souris
+   if (this.input.mousePointer.isDown) {
+     this.tirerBalle();
+   }
+
+   // Passage aux niveaux suivants selon la porte touchée
+   if (Phaser.Input.Keyboard.JustDown(clavier.space) == true) {
+     if (this.physics.overlap(player, this.porte1))
+       this.scene.switch("niveau1"); 
+     if (this.physics.overlap(player, this.porte2))
+       this.scene.switch("niveau2");
+     if (this.physics.overlap(player, this.porte3))
+       this.scene.switch("niveau3");
+   }
+
+
+
+   // Calcul de la direction entre le joueur et la position de la souris
+   let diffX = this.input.mousePointer.worldX - player.x;
+   let diffY = this.input.mousePointer.worldY - player.y;
+
+   // Calcul de l'angle en radians entre le joueur et la souris
+   let angle = Math.atan(diffY / diffX);
+
+   // Convertir l'angle en degrés
+   angle = Phaser.Math.RadToDeg(angle);
+
+   // Ajuster l'angle en fonction de la position du curseur
+   if (diffX < 0) {
+   // player.flipX=true;
+   angle-=180;
+   }
+   else {
+   // player.flipX=false;
+   }
+   // Appliquer la sensibilité à l'angle
+   //angle *= sensitivity;
+
+   // Appliquer la rotation à l'image du joueur
+   player.setAngle(angle);
+   //console.log(angle);
+}
+
+
+
+
+
+ // Fonction pour tirer une balle
+ // Fonction pour tirer une balle
+tirerBalle() {
+ // Calcul du coefficient de direction en fonction de la position du clic de la souris
+ let diffX = this.input.mousePointer.worldX - player.x;
+ let diffY = this.input.mousePointer.worldY - player.y;
+ let distance = Math.sqrt(diffX * diffX + diffY * diffY);
+ let coefdirX = diffX / distance;
+ let coefdirY = diffY / distance;
+
+ // Création de la balle à la position du joueur
+ let bullet = this.physics.add.sprite(player.x + 20 * coefdirX, player.y + 20 * coefdirY, "fireball");
+
+ // Déplacement de la balle vers la position de la souris
+ this.physics.moveTo(
+   bullet,
+   this.input.mousePointer.worldX,
+   this.input.mousePointer.worldY,
+   500
+ );
+ // Gestion des collisions de la balle avec les plateformes
+ this.physics.add.collider(bullet, decor, () => {
+   bullet.destroy();
+ });
+ this.physics.add.collider(bullet, CalquedeTuiles, () => {
+   bullet.destroy();
+ });
+ this.physics.add.collider(bullet, rien, () => {
+  bullet.destroy();
+});
+}
+
+dash (player, image_sprint) {
+ if (player.peutDash == true) {
+     
+     player.peutDash = false; // on désactive la possibilté de dash
+     
+     image_sprint = this.add.image(16, 16, "Sprinter_rouge");
+     
+     // on la réactive dans 4 secondes avec un timer
+     var timerDashOk = this.time.delayedCall(4000, () => {
+         player.peutDash = true;
+         image_sprint = this.add.image(16, 16, "Sprinter_bleu");
+     }, null, this);  
+ }
+}
 }
